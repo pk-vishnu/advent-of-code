97c1268cf27a79dee77d26f96b6f0d4abf812e53
pk-vishnu <vishnuvpk05@gmail.com>
2025-12-04 22:09:21 +0530

    day3 pt2

--- 2025/Day3.java
+++ 2025/Day3.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,14 +6,14 @@ public class Day3 {
   public static void main(String[] args) throws FileNotFoundException {
     File obj = new File("./inputs/day3.txt");
 
-    int result = 0;
+    long result = 0;
 
     try (Scanner scn = new Scanner(obj)) {
       while (scn.hasNextLine()) {
 
-        int best = 0;
+        long best = 0;
         char[] input = scn.nextLine().toCharArray();
-        //
+
         // for (int i = 0; i < input.length - 1; i++) {
         // int first = input[i] - '0';
         // for (int j = i + 1; j < input.length; j++) {
@@ -25,23 +24,45 @@ public class Day3 {
         // }
         // result += best;
 
-        int right_best = 0;
+        // int right_best = 0;
+        //
+        // for (int i = input.length - 1; i >= 0; i--) {
+        // if (right_best == 0) {
+        // right_best = input[i] - '0';
+        // continue;
+        // }
+        //
+        // int left = input[i] - '0';
+        // int right = right_best;
+        // int num = left * 10 + right;
+        //
+        // best = Integer.max(best, num);
+        //
+        // if (left > right) {
+        // right_best = left;
+        // }
+        // }
 
-        for (int i = input.length - 1; i >= 0; i--) {
-          if (right_best == 0) {
-            right_best = input[i] - '0';
-            continue;
+        // part 2
+        int n = input.length;
+        Stack<Integer> stk = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+          int digit = input[i] - '0';
+          int remaining = n - i - 1;
+
+          while (!stk.isEmpty() &&
+              stk.peek() < digit &&
+              (stk.size() + remaining >= 12)) {
+            stk.pop();
           }
 
-          int left = input[i] - '0';
-          int right = right_best;
-          int num = left * 10 + right;
-
-          best = Integer.max(best, num);
-
-          if (left > right) {
-            right_best = left;
+          if (stk.size() < 12) {
+            stk.push(digit);
           }
+        }
+        for (int num : stk) {
+          best = best * 10 + num;
         }
         result += best;
       }
